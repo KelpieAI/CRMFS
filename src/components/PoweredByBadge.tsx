023d52c5050ecb8fd2a4e3fd8491c952a615ba06
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-29 00:29:59 +0000

    Updated PoweredByBadge.tsx

--- src/components/PoweredByBadge.tsx
+++ src/components/PoweredByBadge.tsx
@@ -1,6 +1,6 @@
 export default function PoweredByBadge() {
   // Manually update this number when you make changes
-  const buildNumber = 251; // <-- Just increment this yourself
+  const buildNumber = 259; // <-- Just increment this yourself
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
