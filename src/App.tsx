290ca98d2803fa93b8eb22b208961ac7eeded29c
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-30 09:05:27 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tantml:tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider } from './contexts/ToastContext';
 import ErrorBoundary from './components/ErrorBoundary';
 import CompactLayout from './components/CompactLayout';
