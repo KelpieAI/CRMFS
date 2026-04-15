f50d9a7d47bbd50ad84856f502e804c7afa60081
Sami Mustafa <56640056+KelpieAI@users.noreply.github.com>
2026-04-15 07:19:49 +0100

    Updated version.ts

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
-export const BUILD_NUMBER = 522;
-export const VERSION_STRING = `v0.10.0.${BUILD_NUMBER}`;
-export const FULL_VERSION_STRING = `Version 0.10.0.${BUILD_NUMBER}`;
+export const BUILD_NUMBER = 528;
+export const VERSION_STRING = `v0.10.1.${BUILD_NUMBER}`;
+export const FULL_VERSION_STRING = `Version 0.10.1.${BUILD_NUMBER}`;
